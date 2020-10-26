062ce69cd2cdef86a997718b56e34a302c53a6ba
epaschke22 <ericksonpaschke@gmail.com>
2020-10-26 18:03:54 +0000

    adding interation of int operator functions

--- func_int_ops.c
+++ func_int_ops.c
@@ -1,4 +1,23 @@
+#include "holberton.h"
+
 char *op_int(int n)
+{
+	char *ptr, *str;
+	int len;
+
+	str = _itoa(n);
+	ptr = malloc(_strlen(str) * sizeof(char));
+
+	_strcpy(str, ptr);
+	free(str);
+	return (ptr);
+}
 char *op_uint(unsigned int n)
+{
+}
 char *op_binary(unsigned int n)
+{
+}
 char *op_octal(unsigned int n)
+{
+}
